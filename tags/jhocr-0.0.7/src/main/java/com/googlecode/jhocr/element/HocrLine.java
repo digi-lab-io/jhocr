77b300a5fa0e2d07dba4e1dd18826d3c84deec51
osoriojaques@gmail.com <osoriojaques@gmail.com@f0618af2-38bb-e337-77b0-76f7770e9caa>
2013-12-17 20:30:45 +0000

    [maven-release-plugin]  copy for tag jhocr-0.0.7

---
+++ tags/jhocr-0.0.7/src/main/java/com/googlecode/jhocr/element/HocrLine.java
@@ -0,0 +1,142 @@
+/**
+ * Copyright (©) 2013 Pablo Filetti Moreira
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.googlecode.jhocr.element;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.googlecode.jhocr.attribute.BBox;
+
+/**
+ * Class used to store information "ocr_line" element and their children.
+ * 
+ * Element example: {@code <span class='ocr_line' id='line_4' title="bbox 36 194 585 225">}
+ */
+public class HocrLine extends HocrElement {
+
+	public static final String	TAGNAME		= "span";
+	public static final String	CLASSNAME	= "ocr_line";
+
+	private HocrParagraph		paragraph;
+	private List<HocrWord>		words		= new ArrayList<HocrWord>();
+
+	/**
+	 * Constructs an <code>HocrLine</code> with a unique id and a coordinates <code>BBox</code>.
+	 * 
+	 * @param id
+	 *            Set the id of element.
+	 * @param bbox
+	 *            Sets the coordinates of element.
+	 */
+	public HocrLine(String id, BBox bbox) {
+		super(id, bbox);
+	}
+
+	@Override
+	public String getClassName() {
+		return CLASSNAME;
+	}
+
+	@Override
+	public String getTagName() {
+		return TAGNAME;
+	}
+
+	/**
+	 * @return The children <code>HocrWord</code> of this.
+	 */
+	public List<HocrWord> getWords() {
+		return words;
+	}
+
+	/**
+	 * Set the children <code>HocrWord</code> of this.
+	 * 
+	 * @param words
+	 *            The children <code>HocrWord</code> of this.
+	 */
+	public void setWords(List<HocrWord> words) {
+		this.words = words;
+	}
+
+	/**
+	 * Add new word.
+	 * 
+	 * @param word
+	 *            The new word.
+	 */
+	public void addWord(HocrWord word) {
+		word.setLine(this);
+		getWords().add(word);
+	}
+
+	/**
+	 * @return the {@link #paragraph} object.
+	 */
+	public HocrParagraph getParagraph() {
+		return paragraph;
+	}
+
+	/**
+	 * 
+	 * @param paragraph
+	 *            will be set.
+	 */
+	public void setParagraph(HocrParagraph paragraph) {
+		this.paragraph = paragraph;
+	}
+
+	/**
+	 * @return all words, concatenating children words with space.
+	 */
+	public String getText() {
+
+		StringBuilder text = new StringBuilder();
+
+		boolean first = true;
+
+		/**
+		 * Iterate all children words and build a <code>String</code> concatenating children words with space.
+		 */
+		for (HocrWord word : getWords()) {
+			if (first) {
+				text.append(word.getText());
+				first = false;
+			} else {
+				text.append(" ").append(word.getText());
+			}
+		}
+
+		return text.toString();
+	}
+
+	/**
+	 * 
+	 * @return the informations of this element as a <code>String</code>.
+	 * 
+	 *         <pre>
+	 * {
+	 * 	return &quot;HocrLine{&quot; + super.toString() + &quot;, words=&quot; + words.size() + &quot;}&quot;;
+	 * }
+	 * </pre>
+	 */
+	@Override
+	public String toString() {
+		return "HocrLine{" + super.toString() + ", words=" + words.size() + "}";
+	}
+}
---
+++ tags/jhocr-0.0.7/src/test/java/com/googlecode/jhocr/TestJHOCRConverter.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (©) 2013 Pablo Filetti Moreira & O.J. Sousa Rodrigues
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.googlecode.jhocr;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.googlecode.jhocr.converter.HocrToPdf;
+import com.googlecode.jhocr.util.JHOCRUtil;
+import com.googlecode.jhocr.util.LoggUtilException;
+import com.googlecode.jhocr.util.enums.FExt;
+import com.googlecode.jhocr.util.enums.PDFF;
+
+/**
+ * TODO add documentation
+ * TODO implement tests for all supported formats
+ * 
+ */
+@RunWith(value = Parameterized.class)
+public class TestJHOCRConverter {
+
+	/**
+	 * 
+	 * @param testFileName
+	 *            without the file extension.
+	 */
+	public TestJHOCRConverter(String testFileName) {
+		this.testFileName = testFileName;
+	}
+
+	@Parameters
+	public static Collection<String[]> data() {
+		String[][] data = new String[][] { { "eurotext_tesseract_bin" }, { "eurotext" }, { "phototest" }, { "eurotext_tess4j" } };
+		return Arrays.asList(data);
+	}
+
+	private String				testFileName		= "";
+	private static String		testFileResultsPath	= "src/test/resources/test-results";
+	private static String		testFilesSrcPath	= "src/test/resources/test-data";
+	private final static Logger	logger				= LoggerFactory.getLogger(new LoggUtilException().toString());
+
+	/**
+	 * TODO add documentation
+	 * 
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+
+		String packageFolder = "src/test/resources/test-results";
+		String fileName = "tess4j-jhocr-pdf.pdf";
+
+	}
+
+	/**
+	 * TODO add documentation
+	 * TODO re-implemt this test to not only create an PDF with metadata such as bookmarks, but also to test against it if those were really added to the PDF.
+	 * 
+	 * @throws FileNotFoundException
+	 * 
+	 * @throws Throwable
+	 */
+	@Test
+	public void test() {
+
+		File htmlOcrAbsFileName = new File(testFilesSrcPath, String.format("%s.%s", testFileName, FExt.HTML));
+		File imageAbsFileName = new File(testFilesSrcPath, String.format("%s.%s", testFileName, FExt.TIF));
+		File pdfAbsFileName = new File(testFileResultsPath, String.format("%s.%s", testFileName, FExt.PDF));
+
+		String word = "a";
+
+		try {
+			FileOutputStream os = new FileOutputStream(pdfAbsFileName);
+
+			HocrToPdf hocrToPdf = new HocrToPdf(os);
+			hocrToPdf.addHocrDocument(new FileInputStream(htmlOcrAbsFileName), new FileInputStream(imageAbsFileName));
+			hocrToPdf.setPdfFormat(PDFF.PDF_A_1B);
+			hocrToPdf.convert();
+
+			os.close();
+
+			if (!JHOCRUtil.getInstance().testGeneratedPDF(word, pdfAbsFileName.getAbsolutePath())) {
+				fail("The PDF is not searchable, see the log for more information.");
+			}
+
+		} catch (IOException e) {
+			logger.error("I/O error, please check the log for more information.", e);
+		}
+
+	}
+}
