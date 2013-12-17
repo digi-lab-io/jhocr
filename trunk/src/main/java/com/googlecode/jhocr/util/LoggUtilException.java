e24bc8a84ce35b1e15f5631c9846f752f21b8cab
OsorioJaques@gmail.com <OsorioJaques@gmail.com@f0618af2-38bb-e337-77b0-76f7770e9caa>
2013-12-17 21:33:32 +0000

    remove comment

--- trunk/src/main/java/com/googlecode/jhocr/util/LoggUtilException.java
+++ trunk/src/main/java/com/googlecode/jhocr/util/LoggUtilException.java
@@ -1,8 +1,5 @@
 package com.googlecode.jhocr.util;
 
-/**
- * Please give me and the apache community a call, if you know a better and elegant way to do this!
- */
 public class LoggUtilException extends Exception {
 
 	private static final long	serialVersionUID	= 238204279367101127L;
