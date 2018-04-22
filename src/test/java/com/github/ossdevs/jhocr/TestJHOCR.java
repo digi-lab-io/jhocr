889a5697e93fd62f4b9ba8e48373f273d1f9f299
OJ <osoriojaques@gmail.com>
2018-04-22 21:17:45 +0200

    chore: adapting unit test and removing deprecated api

--- src/test/java/com/github/ossdevs/jhocr/TestJHOCR.java
+++ src/test/java/com/github/ossdevs/jhocr/TestJHOCR.java
@@ -17,34 +17,38 @@
 
 package com.github.ossdevs.jhocr;
 
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.ossdevs.jhocr.converter.HocrToPdf;
-import com.github.ossdevs.jhocr.element.HocrDocument;
 import com.github.ossdevs.jhocr.parser.HocrParser;
 import com.github.ossdevs.jhocr.util.JHOCRUtil;
 import com.github.ossdevs.jhocr.util.LoggUtilException;
 import com.github.ossdevs.jhocr.util.enums.FExt;
 import com.github.ossdevs.jhocr.util.enums.PDFF;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.MethodRule;
-import org.junit.rules.TestWatchman;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-import org.junit.runners.model.FrameworkMethod;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.*;
-import java.util.Arrays;
-import java.util.Collection;
-
-import static org.junit.Assert.fail;
 
 /**
- * TODO implement here all tests around jhocr and the tesseract binaries.
- * For more information, visit: <li>https://code.google.com/p/jhocr/wiki/HowToUse</li>
+ * TODO implement here all tests around jhocr and the tesseract binaries. For
+ * more information, visit:
+ * <li>https://code.google.com/p/jhocr/wiki/HowToUse</li>
  */
 @RunWith(value = Parameterized.class)
 public class TestJHOCR {
@@ -54,21 +58,24 @@ public class TestJHOCR {
     private static String testFilesSrcPath = "src/test/resources/test-data";
     private String testFileName = "";
     /**
-     * This method will be called whenever a test method is entered, helping to log the information and to know which test is currently running.
+     * This method will be called whenever a test method is entered, helping to log
+     * the information and to know which test is currently running.
      */
     @Rule
-    public MethodRule watchman = new TestWatchman() {
-        @Override
-        public void starting(FrameworkMethod method) {
-            logger.info("* {} being run for '{}'.", method.getName(), testFileName);
-        }
+    public TestWatcher watchman = new TestWatcher() {
+	@Override
+	protected void starting(Description description) {
+	    logger.info("* {} being run for '{}'.", description.getMethodName(), testFileName);
+	}
+
     };
 
     /**
-     * @param testFileName without the file extension.
+     * @param testFileName
+     *            without the file extension.
      */
     public TestJHOCR(String testFileName) {
-        this.testFileName = testFileName;
+	this.testFileName = testFileName;
     }
 
     /**
@@ -78,13 +85,13 @@ public class TestJHOCR {
      */
     @Parameters
     public static Collection<String[]> data() {
-        String[][] data = new String[][]{{"eurotext"}, {"phototest"}, {"eurotext_tess4j"}};
-        return Arrays.asList(data);
+	String[][] data = new String[][] { { "eurotext" }, { "phototest" }, { "eurotext_tess4j" } };
+	return Arrays.asList(data);
     }
 
     /**
-     * Test setup and preparation.
-     * TODO delete all files inside the <code>testFileResultsPath</code>.
+     * Test setup and preparation. TODO delete all files inside the
+     * <code>testFileResultsPath</code>.
      */
     @Before
     public void setUp() {
@@ -94,72 +101,57 @@ public class TestJHOCR {
     /**
      * This will test the manual generated test-files from an tesseract-bin.
      *
-     * @param testFileName without the file extension.
+     * @param testFileName
+     *            without the file extension.
      */
     @Test
     public void testManualGeneratedTestFiles() {
 
-        File htmlOcrAbsFileName = new File(testFilesSrcPath, String.format("%s.%s", testFileName, FExt.HTML));
-        File imageAbsFileName = new File(testFilesSrcPath, String.format("%s.%s", testFileName, FExt.TIF));
-        File pdfAbsFileName = new File(testFileResultsPath, String.format("%s.%s", testFileName, FExt.PDF));
+	File htmlOcrAbsFileName = new File(testFilesSrcPath, String.format("%s.%s", testFileName, FExt.HTML));
+	File imageAbsFileName = new File(testFilesSrcPath, String.format("%s.%s", testFileName, FExt.TIF));
+	File pdfAbsFileName = new File(testFileResultsPath, String.format("%s.%s", testFileName, FExt.PDF));
 
-        String word = "a";
+	String word = "a";
 
-        try {
-            FileOutputStream os = new FileOutputStream(pdfAbsFileName);
+	try {
+	    FileOutputStream os = new FileOutputStream(pdfAbsFileName);
 
-            HocrToPdf hocrToPdf = new HocrToPdf(os);
-            hocrToPdf.addHocrDocument(new FileInputStream(htmlOcrAbsFileName), new FileInputStream(imageAbsFileName));
-            hocrToPdf.setPdfFormat(PDFF.PDF_A_3U);
-            hocrToPdf.convert();
-            os.close();
+	    HocrToPdf hocrToPdf = new HocrToPdf(os);
+	    hocrToPdf.addHocrDocument(new FileInputStream(htmlOcrAbsFileName), new FileInputStream(imageAbsFileName));
+	    hocrToPdf.setPdfFormat(PDFF.PDF_A_3U);
+	    hocrToPdf.convert();
+	    os.close();
 
-            if (!JHOCRUtil.getInstance().testGeneratedPDF(word, pdfAbsFileName.getAbsolutePath())) {
-                fail("The PDF is not searchable, see the log for more information.");
-            }
+	    if (!JHOCRUtil.getInstance().testGeneratedPDF(word, pdfAbsFileName.getAbsolutePath())) {
+		fail("The PDF is not searchable, see the log for more information.");
+	    }
 
-        } catch (IOException e) {
-            logger.error("I/O error, please check the log for more information.", e);
-        }
+	} catch (IOException e) {
+	    logger.error("I/O error, please check the log for more information.", e);
+	}
 
     }
 
     /**
-     * TODO re-implement this test, printing it out is pretty ugly and does not test anything.
+     * TODO re-implement this test, printing it out is pretty ugly and does not test
+     * anything.
      *
      * @throws FileNotFoundException
      */
     @Test
     public void testParser() throws FileNotFoundException {
-        try {
+	try {
 
-            File htmlOcrAbsFileName = new File(testFilesSrcPath, String.format("%s.%s", testFileName, FExt.HTML));
-            HocrParser parser = new HocrParser(new FileInputStream(htmlOcrAbsFileName));
+	    File htmlOcrAbsFileName = new File(testFilesSrcPath, String.format("%s.%s", testFileName, FExt.HTML));
+	    HocrParser parser = new HocrParser(new FileInputStream(htmlOcrAbsFileName));
+	    logger.info("TODO re-implement this test, printing it out is pretty ugly and does not test anything.");
 
-            HocrDocument document = parser.parse();
-            logger.info("TODO re-implement this test, printing it out is pretty ugly and does not test anything.");
+	} catch (FileNotFoundException e) {
+	    logger.error(
+		    "It seams, that the file does not exists or were delete before accessing it, check the log for more information.",
+		    e);
 
-            // String pre = "-- ";
-            // for (HocrPage page : document.getPages()) {
-            // System.out.println(pre + page);
-            // for (HocrCarea carea : page.getCareas()) {
-            // System.out.println(pre + pre + carea);
-            // for (HocrParagraph paragraph : carea.getParagraphs()) {
-            // System.out.println(pre + pre + pre + paragraph);
-            // for (HocrLine line : paragraph.getLines()) {
-            // System.out.println(pre + pre + pre + pre + line);
-            // for (HocrWord word : line.getWords()) {
-            // System.out.println(pre + pre + pre + pre + pre + word);
-            // }
-            // }
-            // }
-            // }
-            // }
-
-        } catch (FileNotFoundException e) {
-            logger.error("It seams, that the file does not exists or were delete before accessing it, check the log for more information.", e);
-
-        }
+	}
     }
 
 }
